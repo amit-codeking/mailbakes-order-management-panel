c1d03be652bc173c97fdd1ae15cc69e8ffaadacc
amit-codeking <amitsuthar.expert@gmail.com>
2026-03-30 16:25:52 +0530

    pm2 config added

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "shopify-order-front",
+      cwd: "/home/ubuntu/front-actions-runner/_work/mailbakes-order-management-panel/mailbakes-order-management-panel",
+      script: "npm",
+      args: "start",
+     
+    },
+  ],
+};
